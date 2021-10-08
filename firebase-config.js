d519874b9491f9b4b472fb0ad7349d4b2b05510e
Arjun8485 <64953034+Arjun8485@users.noreply.github.com>
2021-10-08 23:18:40 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAZZbSd-Uts217L1B_-QIdetluOWeKxc7I",
+    authDomain: "goge-africa-3a3e7.firebaseapp.com",
+    projectId: "goge-africa-3a3e7",
+    storageBucket: "goge-africa-3a3e7.appspot.com",
+    messagingSenderId: "523117592570",
+    appId: "1:523117592570:web:f78e3557764100fee66e43"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
